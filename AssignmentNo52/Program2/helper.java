bd489cf8e6d6c5b389749caf6448d785ec260304
prajwalkadam7020 <81584701+prajwalkadam7020@users.noreply.github.com>
2021-06-13 23:58:30 +0530

    Add files via upload

---
+++ AssignmentNo52/Program2/helper.java
@@ -0,0 +1,95 @@
+import java.util.*;
+
+
+ /*  Problem Statement-
+
+        Write a program which accept matrix from user and return addition of diagonal elements. 
+        Input : 
+                3  2  5  9 
+                4  3  2  2 
+                8  4  1  5
+                3  9  7  5  
+
+        Output : 12 
+                                                               
+*/
+import java.util.*;
+
+class Matrix
+{
+     public int Arr[][];
+     public Matrix(int iRow,int iCol)
+     {
+          Arr = new int[iRow][iCol];
+     }
+     protected void finalize()
+     {
+          Arr = null;
+     }
+
+     public void Accept()
+     {
+          Scanner sobj = new Scanner(System.in);
+
+          System.out.println("Enter the elements:");
+          for(int i = 0; i <Arr.length; i++)
+          {
+               for(int j = 0; j < Arr[i].length; j++)
+               {
+                    Arr[i][j] = sobj.nextInt();
+               }
+          }
+     }
+     public void Display()
+     {
+          System.out.println("Entered elements are:");
+          for(int i = 0; i <Arr.length;i++)
+          {
+               for(int j = 0 ; j < Arr[i].length ;j++)
+               {
+                    System.out.print(Arr[i][j]+"\t");
+               }
+               System.out.println();
+          }
+     }
+
+     public int AddDiagonal(int iNo) 
+     {
+		int iCnt = 0;
+		for(int i = 0; i < Arr.length; i++)	// Rows
+		{
+			for(int j = 0; j < Arr[i].length; j++)	// Columns
+			{
+                    if(Arr[i][j]==iNo)
+                    {
+                         iCnt++;
+                    }
+			}
+		}
+		return iCnt;
+     } 
+}
+
+class helper
+{
+     public static void main(String arg[])
+     {
+        
+          Scanner sobj = new Scanner(System.in);
+          System.out.println("Enter Number of rows");
+          int row = sobj.nextInt();
+
+          System.out.println("Enter number of columns");
+          int col = sobj.nextInt();
+
+
+          Matrix mobj = new Matrix(row,col);
+          mobj.Accept();
+          mobj.Display();
+          System.out.println("Enter Number for Count Frequency:");
+          int iNo = sobj.nextInt();
+          int iRet = mobj.AddDiagonal(iNo);
+          System.out.println("Frequency of number is:"+iRet);
+          System.gc();
+     }
+}
---
+++ AssignmentNo52/Program4/helper.java
@@ -0,0 +1,86 @@
+import java.util.*;
+
+/*  Problem Statement-
+
+        Write a program which accept matrix from user and return addition of diagonal elements. 
+        Input : 
+                3  2  5  9 
+                4  3  2  2 
+                8  4  1  5
+                3  9  7  5  
+
+     Output : 18 18 15 25
+                                                               
+*/
+
+import java.util.*;
+class Matrix
+{
+     public int Arr[][];
+     public Matrix(int iRow,int iCol)
+     {
+          Arr = new int[iRow][iCol];
+     }
+     protected void finalize()
+     {
+          Arr = null;
+     }
+     public void Accept()
+     {
+          Scanner sobj = new Scanner(System.in);
+
+          System.out.println("Enter the elements:");
+          for(int i = 0; i <Arr.length; i++)
+          {
+               for(int j = 0; j < Arr[i].length; j++)
+               {
+                    Arr[i][j] = sobj.nextInt();
+               }
+          }
+     }
+     public void Display()
+     {
+          System.out.println("Entered elements are:");
+          for(int i = 0; i <Arr.length;i++)
+          {
+               for(int j = 0 ; j < Arr[i].length ;j++)
+               {
+                    System.out.print(Arr[i][j]+"\t");
+               }
+               System.out.println();
+          }
+     }
+     public void  AddColumn() 
+     {
+		int iSum = 0;
+		for(int i = 0; i < Arr.length; i++)	// Rows
+		{
+			for(int j = 0; j < Arr[i].length; j++)	// Columns
+			{
+                    iSum = iSum + Arr[j][i];
+          	}
+               System.out.println("Addition of row no "+ (i+1) +" is : "+iSum );
+               iSum = 0;
+          }
+
+     } 
+}
+class helper
+{
+     public static void main(String arg[])
+     {        
+          Scanner sobj = new Scanner(System.in);
+          System.out.println("Enter Number of rows");
+          int row = sobj.nextInt();
+
+          System.out.println("Enter number of columns");
+          int col = sobj.nextInt();
+
+
+          Matrix mobj = new Matrix(row,col);
+          mobj.Accept();
+          mobj.Display();
+          mobj.AddColumn();
+          System.gc();
+     }
+}
